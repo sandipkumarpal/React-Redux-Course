30ce56d168a403d085359769f481a9837b08815a
Sandip Pal <sandip.pal18@gmail.com>
2017-12-09 13:43:32 +0530

    Added component & containers folder and files added PropTypes package

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ScoreBoard from './src/containers/ScoreBoard';
+
+ReactDOM.render(
+    <ScoreBoard />,
+    document.getElementById('root')
+  );
+
